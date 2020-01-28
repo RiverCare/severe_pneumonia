a794a7dac2cf9d3b32836baad7be5773828e1b1e
Cheng Lin <mhclin113@gmail.com>
2020-01-29 01:42:58 +0800

    add spring boot app

---
+++ src/main/java/com/pneumonia/production/SeverePneumoniaApplication.java
@@ -0,0 +1,13 @@
+package com.pneumonia.production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverePneumoniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverePneumoniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pneumonia/production/SeverePneumoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pneumonia.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverePneumoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
